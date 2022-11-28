b6b3220709b577b5945b1aa48486b01a49dda61d
Praveen <praveennanda124@gmail.com>
2022-11-28 16:35:45 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import App from './components/App';
+import Cart from './components/Cart';
+import BaseLayout from './components/BaseLayout';
+import Women from './components/Women';
+import Men from './components/Men';
+import Clothes from './components/Clothes';
+import Accessories from './components/Accessories';
+import ShowProduct from './components/ShowProduct';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <BaseLayout>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/cart" component={Cart} />
+        <Route path="/women" component={Women}  />
+        <Route path="/men" component={Men} />
+        <Route path="/clothes" component={Clothes}  />
+        <Route path="/accessories" component={Accessories} />
+        <Route exact path="/products/:id" component={ShowProduct} />
+      </Switch>
+    </BaseLayout>
+  </BrowserRouter>
+
+, document.getElementById('root'));
+registerServiceWorker();
